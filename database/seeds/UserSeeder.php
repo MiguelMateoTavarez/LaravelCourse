a12110ab385dcf1d3eaf797fccbc8cabbb165f87
Miguel Mateo <miguelmateot94@gmail.com>
2021-10-30 20:11:37 -0400

    Factory

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,11 +19,18 @@ class UserSeeder extends Seeder
         $professionId = Profession::where('title', 'Desarrollador back-end')
                         ->value('id');
         
-        User::create([
+        factory(User::class)->create([
             'name' => 'Miguel Mateo',
             'email' => 'miguel@mateo.com',
             'password' => Hash::make('laravel'),
-            'profession_id' => $professionId
+            'profession_id' => $professionId,
+            'is_admin' => true,
         ]);
+
+        factory(User::class)->create([
+            'profession_id' => $professionId,
+        ]);
+
+        factory(User::class, 48)->create();
     }
 }
